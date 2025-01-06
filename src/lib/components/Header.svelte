45f9e6e827e409a6f4d9bf50fce23755ec1a6081
Meggi <marketal.zemanova@live.com>
2025-01-06 14:40:47 +0100

    unnötiges HTML entfernt

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,3 +1,1 @@
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-<div class="bg-red-500 text-white p-4 ">This is a red box</div>

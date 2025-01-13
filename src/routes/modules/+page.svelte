4626bd4f2f4ca2d4cd5a5bb014725d527ff45027
Ileies <admin@ileies.de>
2025-01-13 11:36:51 +0100

    Added all modules page

---
+++ src/routes/modules/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+import ModuleGrid from '$lib/components/ModuleGrid.svelte';
+import cms from '$lib/cms.svelte';
+</script>
+
+<div class="mt-32">
+	<ModuleGrid modules={cms.modules.list} />
+</div>
